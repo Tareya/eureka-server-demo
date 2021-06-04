6404c4eb07988f775896620df1048be471546a25
沈磊 <shen.lei@uniondrug.com>
2021-06-04 11:20:35 +0800

    Initial commit

---
+++ src/test/java/com/elvin/example/eurekaserverdemo/EurekaServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elvin.example.eurekaserverdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
